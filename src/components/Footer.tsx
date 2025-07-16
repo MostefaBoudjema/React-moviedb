f7189278a3d86e5ce42a5f112b3dbb65527267ed
mostfa <myreal17@gmail.com>
2025-07-16 21:33:14 +0100

    change name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
         <div className="sticky-footer py-1">
             <div className="container">
                 <div className="footer-content d-flex align-items-center justify-content-center">
-                    <strong>Moviez</strong> - 1.2.1 - 2025
+                    <strong>MyMoviez</strong> - 1.2.1 - 2025
                 </div>
             </div>
         </div>
